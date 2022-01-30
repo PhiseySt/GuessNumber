75c37a76edb8e46f88e3bfe449c59f51c322c156
FiseyskiySV <stanislav.fiseyskiy@akbarsdigital.ru>
2022-01-30 10:49:23 +0300

    First init

---
+++ InitGame.cs
@@ -0,0 +1,40 @@
+﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+
+namespace GuessNumber
+{
+    internal class InitGame
+    {
+        private readonly ScriptEngine _pythonEngine;
+        private dynamic _game;
+
+        public InitGame()
+        {
+            _pythonEngine = Python.CreateEngine();
+        }
+
+        public void StartGameInstance()
+        {
+            _pythonEngine.SetSearchPaths(new[] { "PythonScripts" }); //which folder
+            var scope = _pythonEngine.CreateScope(); //create a scope for the engine
+            scope.ImportModule("GuessingGame"); //import the module
+            _pythonEngine.Execute("game = GuessingGame.GuessingGame()", scope); //create an object (module.class)
+            _game = scope.GetVariable("game"); //get the object   
+
+            var rand = new Random(DateTime.Now.Millisecond);
+            var randomNumber = rand.Next(0, 10);
+            _game.set_number_to_guess(randomNumber);
+
+            var response = _game.start_game();
+            Console.WriteLine(response);
+        }
+
+
+        public void MakeGuess(int number)
+        {
+            var response = _game.make_guess(number).ToString();
+            Console.WriteLine(response);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using GuessNumber;
+
+var game = new InitGame();
+game.StartGameInstance();
+
+var keepPlaying = true;
+
+while (keepPlaying)
+{
+
+    Console.WriteLine("Please make a guess");
+    var guess = int.Parse(Console.ReadLine() ?? string.Empty);
+    game.MakeGuess(guess);
+
+
+    Console.WriteLine("Do you want to guess again? Y/N");
+    var decision = Console.ReadKey();
+    Console.WriteLine(Environment.NewLine);
+
+    if (decision.KeyChar != 'Y' && decision.KeyChar != 'y')
+    {
+        keepPlaying = false;
+    }
+}
+
